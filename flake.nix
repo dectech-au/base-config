c4a71e34a70450c3ac6dbb01074847fc7f7b5f26
DecTec <zozano@protonmail.com>
2025-06-05 12:17:06 +1000

    2025-06-05_12:17:06

--- flake.nix
+++ flake.nix
@@ -15,9 +15,14 @@
      inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, home-manager, nixvim, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nixvim, aagl, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -30,12 +35,23 @@
           inherit system;
           modules = [
             ./configuration.nix
+            { 
+              imports = [ aagl.nixosModules.default ]
+              nix.settings = aagl.nixConfig;
+              programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
+              programs.anime-games-launcher.enable = true;      # Genshin Impact
+              programs.honkers-railway-launcher.enable = true;  # Honkai: Star Rail
+              programs.honkers-launcher.enable = true;          # Honkai: Impact 3rd
+              programs.wavey-launcher.enable = true;            # ???
+              programs.sleepy-launcher.enable = true;           # Zenless Zone Zero
+            }
+            
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.dectec = import ./home.nix;
             }
-	    nixvim.nixosModules.nixvim
+	          nixvim.nixosModules.nixvim
           ];
         };
       };
