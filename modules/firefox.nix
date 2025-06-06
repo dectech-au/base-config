5f5164a11623586ded8775eae3d9da7a4e1af9e1
DecTec <zozano@protonmail.com>
2025-06-06 11:41:53 +1000

    2025-06-06_11:41:53

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,7 +1,11 @@
 #~/.dotfiles/modules/firefox.nix
 { config, lib, pkgs, ... }:
 {
-	programs.firefox = {
+	environment.systemPackages = with pkgs; [
+    nurl
+  ];
+
+  programs.firefox = {
     enable = true;
     profiles.default = {
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
