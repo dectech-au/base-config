511930b24e42dfb22599baa5d1ce5e4c60a8b325
DecTec <zozano@protonmail.com>
2025-06-06 11:28:53 +1000

    2025-06-06_11:28:53

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,5 +1,22 @@
 #~/.dotfiles/modules/firefox.nix
 { config, lib, pkgs, ... }:
 {
-	programs.firefox.enable = true;
+	programs.firefox = {
+    enable = true;
+    default.extensions {
+      packages = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+      ];
+      settings."uBlock@raymondhill.net".settings = {
+        selectedFilterLists = [
+          "ublock-filters"
+          "ublock-badware"
+          "ublock-privacy"
+          "ublock-unbreak"
+          "ublock-quick-fixes"
+        ];
+      };
+    };
+  };
+
 }
