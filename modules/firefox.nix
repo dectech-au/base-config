fcc51e5c121051d3ca46f374b69d87585d4c2dfd
DecTec <zozano@protonmail.com>
2025-06-06 12:20:10 +1000

    2025-06-06_12:20:10

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -3,11 +3,13 @@
 {
   programs.firefox = {
     enable = true;
-    # profiles.default = {
+    profiles.default = {
+      name = "default";
+      isDefault = true;
     #   extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
     #     ublock-origin
     #   ];
     #   settings.extensions.autoDisableScopes = 0;
-    # };
+    };
   };
 }
