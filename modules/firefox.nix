95459fccbe612f6571a1dbca2fbd363e0584bf13
DecTec <zozano@protonmail.com>
2025-06-06 11:33:27 +1000

    2025-06-06_11:33:27

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -4,9 +4,7 @@
 	programs.firefox = {
     enable = true;
     profiles.default.extensions.packages = [
-      "uBlock0@raymondhill.net"
-      "https-everywhere@eff.org"
-      "addon@darkreader.org"
+      ublock-origin
     ];
   };
 }
