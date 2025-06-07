ebbd914c5f8442457320ed7b2aa30f260dbb8c16
DecTec <zozano@protonmail.com>
2025-06-07 18:06:57 +1000

    2025-06-07_18:06:57

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,14 +1,12 @@
 #~/.dotfiles/modules/firefox.nix
 { config, lib, pkgs, inputs, ... }:
 
-let
-  firefox-addons = pkgs.callPackage <nixpkgs/pkgs/applications/networking/browsers/firefox/extensions.nix> {};
-in {
+{
   programs.firefox = {
     enable = true;
     profiles.default = {
-      extensions = with pkgs; [
-        nur.repos.rycee.firefox-addons.ublock-origin
+      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+        ublock-origin
       ];
     };
   };
