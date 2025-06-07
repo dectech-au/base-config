f77506daeaf38fb988c71faa63fdf0df7bea3852
DecTec <zozano@protonmail.com>
2025-06-07 14:12:00 +1000

    2025-06-07_14:12:00

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,8 +1,20 @@
 #~/.dotfiles/modules/firefox.nix
 { config, lib, pkgs, inputs, ... }:
+
+  let
+    addons = with pkgs.firefox-addons; [
+      ublock-origin
+    ];
+  in
+  
 {
   programs.firefox = {
     enable = true;
+    profiles.default = {
+      extensions = {
+        packages = addons;
+      };
+    };
   };
 
   programs.librewolf = {
