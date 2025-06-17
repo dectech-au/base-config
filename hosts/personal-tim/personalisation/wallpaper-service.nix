77e292a809386c6555c970fb0a2cf90d3e251bbd
DecTec <zozano@protonmail.com>
2025-06-17 17:52:02 +1000

    2025-06-17_17:52:02

--- hosts/personal-tim/personalisation/wallpaper-service.nix
+++ hosts/personal-tim/personalisation/wallpaper-service.nix
@@ -3,7 +3,13 @@
 {
   systemd.services.set-wallpaper = {
     description = "Set KDE Plasma Wallpaper";
-    serviceConfig.ExecStart = [ "/run/current-system/sw/bin/plasma-apply-wallpaperimage /home/dectec/.dotfiles/hosts/personal-tim/persionalisation/wallpaper.png" ];
-    wantedBy = [ "graphical-session.target" ];
+    #serviceConfig.ExecStart = [ "/run/current-system/sw/bin/plasma-apply-wallpaperimage /home/dectec/.dotfiles/hosts/personal-tim/persionalisation/wallpaper.png" ];
+    wantedBy = [ "multi-user.target" ];
+    after = [ "graphical.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.plasma-workspace}/bin/plasma-apply-wallpaperimage /home/dectec/.dotfiles/hosts/personal-tim/personalisation/wallpaper.png";
+    };
   };
 }
