d8d5ffdc8d2602f167da1d5177ee44934cfbaeef
DecTec <zozano@protonmail.com>
2025-06-17 20:43:08 +1000

    2025-06-17_20:43:08

--- modules/kdeconnect.nix
+++ modules/kdeconnect.nix
@@ -1,5 +1,8 @@
 #~/.dotfiles/modules/kdeconnect.nix
 { config, lib, pkgs, ... }:
 {
-  services.kdeconnect.enable = true;
+  services.kdeconnect = {
+    enable = true;
+    indicator = true;
+  };
 }
