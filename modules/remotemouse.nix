4a7ffaf7de25b8a025a911dc7ba76de0b68fa22e
DecTec <zozano@protonmail.com>
2025-06-17 18:48:30 +1000

    2025-06-17_18:48:30

--- modules/remotemouse.nix
+++ modules/remotemouse.nix
@@ -4,12 +4,27 @@ let
   remotemouse = pkgs.buildFHSEnv {
     name = "remotemouse";
     targetPkgs = pkgs: with pkgs; [
+      dbus
+      freetype
+      fontconfig
       glib
+      gtk3
       libGL
+      python37
+      python37Packages.pyqt5
+      qt5.qtbase
+      qt5.qtwayland
+      qt5.qttools
+      stdenv.cc.cc
       xclip
       xorg.libxcb
       xorg.libX11
       xorg.libXinerama
+      xorg.libXext
+      xorg.libXtst
+      xorg.libXrender
+      xorg.libXrandr
+      xorg.libXfixes
       zlib
       # add other missing libs as needed
     ];
