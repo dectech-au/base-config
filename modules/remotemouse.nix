52ce5dbb7bc9c5fdd31a58af8e06de33f8962db9
DecTec <zozano@protonmail.com>
2025-06-17 21:44:06 +1000

    2025-06-17_21:44:06

--- modules/remotemouse.nix
+++ modules/remotemouse.nix
@@ -1,44 +1,45 @@
 # ~/.dotfiles/modules/remotemouse.nix or similar
 { config, lib, pkgs, ... }:
-let
-  remotemouse-fhs = pkgs.buildFHSEnv {
-    name = "remotemouse";
-    targetPkgs = pkgs: with pkgs; [
-      clip
-      glib
-      gtk3
-      dbus
-      libxkbcommon
-      libGL
-      zlib
-      freetype
-      fontconfig
-      stdenv.cc.cc
+  
+pkgs.buildFHSEnv {
+  name = "remotemouse";
 
-      # Qt and X11 dependencies
-      qt5.qtbase
-      qt5.qttools
-      qt5.qtwayland
-      xorg.xcbutil
-      xorg.xcbutilwm
-      xorg.libxcb
-      xorg.libX11
-      xorg.libXinerama
-      xorg.libXext
-      xorg.libXtst
-      xorg.libXrender
-      xorg.libXrandr
-      xorg.libXfixes
-      xorg.xcbutilimage
-      xorg.xcbutilkeysyms
-      xorg.xcbutilrenderutil
-    ];
+  runScript = "${toString ../../Downloads/RemoteMouse_x86_64/RemoteMouse}";
 
-    # Force xcb platform to avoid wayland issues
-    runScript = ''
-      env QT_QPA_PLATFORM=xcb /home/dectec/.dotfiles/modules/RemoteMouse_x86_64/RemoteMouse
-    '';
-  };
-in {
-  home.packages = [ remotemouse-fhs ];
+  nativeBuildInputs = with pkgs; [
+    clip
+    glib
+    gtk3
+    dbus
+    libxkbcommon
+    libGL
+    zlib
+    freetype
+    fontconfig
+    stdenv.cc.cc
+
+    # Qt and X11 dependencies
+    qt5.qtbase
+    qt5.qttools
+    qt5.qtwayland
+    xorg.xcbutil
+    xorg.xcbutilwm
+    xorg.libxcb
+    xorg.libX11
+    xorg.libXinerama
+    xorg.libXext
+    xorg.libXtst
+    xorg.libXrender
+    xorg.libXrandr
+    xorg.libXfixes
+    xorg.xcbutilimage
+    xorg.xcbutilkeysyms
+    xorg.xcbutilrenderutil
+  ];
+
+  extraPkgs = with pkgs; [];
+
+  shellHook = ''
+    export DISPLAY=$DISPLAY
+  '';
 }
