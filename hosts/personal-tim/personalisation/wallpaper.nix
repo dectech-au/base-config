a43dc736aceef045f19f10b882fec6d305d2b6ac
DecTec <zozano@protonmail.com>
2025-06-17 13:15:48 +1000

    2025-06-17_13:15:48

--- hosts/personal-tim/personalisation/wallpaper.nix
+++ hosts/personal-tim/personalisation/wallpaper.nix
@@ -3,6 +3,6 @@
 {
   home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".text = ''
     [Containments][1][Wallpaper][org.kde.image][General]
-    Image=file:///home/dectec/.dotfiles/hosts/personal-tim/personalisation/wallpaper.jpg
+    Image=file:///home/dectec/.dotfiles/hosts/personal-tim/personalisation/wallpaper.png
   '';
 }
