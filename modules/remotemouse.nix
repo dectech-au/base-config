5627cf7c1cffd0edd558ad10c24905c103930d5d
DecTec <zozano@protonmail.com>
2025-06-17 18:41:26 +1000

    2025-06-17_18:41:26

--- modules/remotemouse.nix
+++ modules/remotemouse.nix
@@ -8,6 +8,7 @@ let
       xorg.libxcb
       xorg.libX11
       xorg.libXinerama
+      zlib
       # add other missing libs as needed
     ];
     runScript = "./modules/RemoteMouse_x86_64/RemoteMouse"; # actual binary
