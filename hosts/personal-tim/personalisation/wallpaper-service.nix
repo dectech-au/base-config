ff3524edbf3031e533408b404747024af568b66b
DecTec <zozano@protonmail.com>
2025-06-17 18:02:02 +1000

    2025-06-17_18:02:02

--- hosts/personal-tim/personalisation/wallpaper-service.nix
+++ hosts/personal-tim/personalisation/wallpaper-service.nix
@@ -2,6 +2,6 @@
 { config, lib, pkgs, ... }:
 {
   home.activation.setWallpaper = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.plasma-workspace}/bin/plasma-apply-wallpaperimage /home/dectec/.dotfiles/hosts/personal-tim/personalisation/wallpaper.png
+    /run/current-system/sw/bin/plasma-apply-wallpaperimage /home/dectec/.dotfiles/hosts/personal-tim/personalisation/wallpaper.png
   '';
 }
