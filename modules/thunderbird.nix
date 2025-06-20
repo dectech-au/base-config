3bfd2aa86e19f76cb7583032ad4ab964fd7c4d29
DecTec <zozano@protonmail.com>
2025-06-21 09:55:54 +1000

    2025-06-21_09:55:54

--- modules/thunderbird.nix
+++ modules/thunderbird.nix
@@ -3,5 +3,14 @@
 {
   programs.thunderbird = {
     enable = true;
+    profiles.default = {
+      isDefault = true;
+      settings = {
+        extensions.autoDisableScopes = "0";
+      };
+      # extensions = [
+      #
+      # ];
+    };
   };
 }
