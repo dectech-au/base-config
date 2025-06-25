98ff0672c6d2bfe9b19b440929cd4cd8693d0c05
DecTec <zozano@protonmail.com>
2025-06-25 10:57:21 +1000

    2025-06-25_10:57:21

--- modules/thunderbird.nix
+++ modules/thunderbird.nix
@@ -13,4 +13,28 @@
       # ];
     };
   };
+
+  xdg.configFile = {
+    "autostart/birdtray.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Exec=birdtray
+      Hidden=false
+      NoDisplay=false
+      X-GNOME-Autostart-enabled=true
+      Name=Birdtray
+      Comment=Thunderbird system tray integration
+    '';
+
+    "autostart/thunderbird.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Exec=thunderbird --silent
+      Hidden=false
+      NoDisplay=false
+      X-GNOME-Autostart-enabled=true
+      Name=Thunderbird
+      Comment=Start Thunderbird at login
+    '';
+  };
 }
