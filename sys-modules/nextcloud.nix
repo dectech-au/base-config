cc9133f1ce94abb7f14d9a9a0754c4071e865c68
DecTec <zozano@protonmail.com>
2025-06-28 11:13:11 +1000

    2025-06-28_11:13:11

--- sys-modules/nextcloud.nix
+++ sys-modules/nextcloud.nix
@@ -1,6 +1,8 @@
 #~/.dotfiles/sys-modules/nextcloud.nix
 { config, lib, pkgs, ... }:
 {
+  environment.etc."nextcloud-admin-pass".text = "nextcloud1234";
+
   services.nextcloud = {
     enable = true;
     hostName = "localhost";
