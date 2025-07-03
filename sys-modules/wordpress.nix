937a093bd248381100f59b9b98ea31f7fc7277d1
DecTec <zozano@protonmail.com>
2025-07-03 19:14:49 +1000

    2025-07-03_19:14:49

--- sys-modules/wordpress.nix
+++ sys-modules/wordpress.nix
@@ -1,21 +1,21 @@
 #~/.dotfiles/sys-modules/wordpress.nix
 { config, lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs.wordpressPackages; [
-    themes.twentytwentythree
-    plugins.antispam-bee
-    plugins.opengraph
-  ];
+  # environment.systemPackages = with pkgs.wordpressPackages; [
+  #   themes.twentytwentythree
+  #   plugins.antispam-bee
+  #   plugins.opengraph
+  # ];
 
-  services.wordpress.webserver = "httpd";
+  # services.wordpress.webserver = "httpd";
 
   services.wordpress.sites."localhost" = {
     virtualHost = {
       hostName = "localhost";
-      # themes = {
-      #   inherit (pkgs.wordpressPackages.themes)
-      #     twentytwentythree;
-      # };
+      themes = {
+        inherit (pkgs.wordpressPackages.themes)
+          twentytwentythree;
+      };
       # plugins = {
       #   inherit (pkgs.wordpressPackages.plugins)
       #     antispam-bee
