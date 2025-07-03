a39d8dc16c712700103cd70ed258c9e014051c59
DecTec <zozano@protonmail.com>
2025-07-03 19:17:10 +1000

    2025-07-03_19:17:10

--- sys-modules/wordpress.nix
+++ sys-modules/wordpress.nix
@@ -1,38 +1,21 @@
 #~/.dotfiles/sys-modules/wordpress.nix
 { config, lib, pkgs, ... }:
 {
-  # environment.systemPackages = with pkgs.wordpressPackages; [
-  #   themes.twentytwentythree
-  #   plugins.antispam-bee
-  #   plugins.opengraph
-  # ];
-
-  # services.wordpress.webserver = "httpd";
-
   services.wordpress.sites."localhost" = {
     virtualHost = {
       hostName = "localhost";
-      themes = {
-        inherit (pkgs.wordpressPackages.themes)
-          twentytwentythree;
-      };
-      # plugins = {
-      #   inherit (pkgs.wordpressPackages.plugins)
-      #     antispam-bee
-      #     opengraph;
-      # };
     };
-    # database = {
-    #   name = "wordpress";
-    #   user = "wp";
-    #   password = "changeme";
-    # };
-    # admin = {
-    #   user = "admin";
-    #   password = "changeme";
-    #   email = "admin@dectech.au";
-    # };
+    
+    themes = {
+      inherit (pkgs.wordpressPackages.themes)
+        twentytwentythree;
+    };
+    
+    plugins = {
+      inherit (pkgs.wordpressPackages.plugins)
+        antispam-bee
+        opengraph;
+    };
   };
-
   networking.hosts."127.0.0.1" = [ "wordpress.local" ];
 }
