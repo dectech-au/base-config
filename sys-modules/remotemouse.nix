245ead41fc69a86e81d8c491b61b5790f2fa1e5a
DecTec <zozano@protonmail.com>
2025-07-11 18:06:45 +1000

    2025-07-11_18:06:45

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -1,35 +1,38 @@
 # ~/.dotfiles/modules/remotemouse.nix or similar
 { config, lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    clip
-    glib
-    gtk3
-    dbus
-    libxkbcommon
-    libGL
-    zlib
-    freetype
-    fontconfig
-    stdenv.cc.cc
-    patchelf
 
-    # Qt and X11 dependencies
-    qt5.qtbase
-    qt5.qttools
-    qt5.qtwayland
-    xorg.xcbutil
-    xorg.xcbutilwm
-    xorg.libxcb
-    xorg.libX11
-    xorg.libXinerama
-    xorg.libXext
-    xorg.libXtst
-    xorg.libXrender
-    xorg.libXrandr
-    xorg.libXfixes
-    xorg.xcbutilimage
-    xorg.xcbutilkeysyms
-    xorg.xcbutilrenderutil
-  ];
+  services.firewall.enableTCPPorts = [ 1978 ];
+  services.firewall.enableUDPPorts = [ 1978 ];
+  # environment.systemPackages = with pkgs; [
+  #   clip
+  #   glib
+  #   gtk3
+  #   dbus
+  #   libxkbcommon
+  #   libGL
+  #   zlib
+  #   freetype
+  #   fontconfig
+  #   stdenv.cc.cc
+  #   patchelf
+  #
+  #   # Qt and X11 dependencies
+  #   qt5.qtbase
+  #   qt5.qttools
+  #   qt5.qtwayland
+  #   xorg.xcbutil
+  #   xorg.xcbutilwm
+  #   xorg.libxcb
+  #   xorg.libX11
+  #   xorg.libXinerama
+  #   xorg.libXext
+  #   xorg.libXtst
+  #   xorg.libXrender
+  #   xorg.libXrandr
+  #   xorg.libXfixes
+  #   xorg.xcbutilimage
+  #   xorg.xcbutilkeysyms
+  #   xorg.xcbutilrenderutil
+  # ];
 }
