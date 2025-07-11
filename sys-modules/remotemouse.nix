af2a67d0405eabb87d238171da2776d6cf096227
DecTec <zozano@protonmail.com>
2025-07-11 20:39:46 +1000

    2025-07-11_20:39:46

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -6,17 +6,13 @@
 # wineserver -k                             ##### restart wine
 
 ##### Install these libraries 
-# winetricks corefonts dinput directplay input8 nocrashdialog vcrun6 
+# winetricks corefonts dinput8 directplay input8 nocrashdialog vcrun2015
 
 ##### Libraries which breaks shit:
 # dotnet40 gdiplus
 
 ##### Force Wine to handle keyboard and mouse input:
-# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "UseTakeFocus" /d "Y" /f
-# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "UseXVidMode" /d "N" /f
-# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "GrabKeyboard" /d "Y" /f
-# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "GrabPointer" /d "Y" /f
-# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "MouseWarpOverride" /d "force" /f
+# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "UseTakeFocus" /d "Y" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "UseXVidMode" /d "N" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "GrabKeyboard" /d "Y" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "GrabPointer" /d "Y" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "MouseWarpOverride" /d "force" /f
 
 ##### Input device permissions:
 # sudo groupadd uinput && sudo usermod -aG input,uinput $(whoami) 
@@ -34,6 +30,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    evtest
     winetricks
     wineWowPackages.staging
   ];
