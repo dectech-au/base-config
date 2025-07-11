b7aa8dff6463a424caf6cf9fd2e9acd98a326f7f
DecTec <zozano@protonmail.com>
2025-07-11 16:22:19 +1000

    2025-07-11_16:22:19

---
+++ sys-modules/nix-ld.nix
@@ -0,0 +1,31 @@
+#~/.dotfiles/sys-modules/nix-ld.nix
+{ config, lib, pkgs, ... }:
+
+let
+  legacyPkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
+    sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+  }) {};
+in
+
+{
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      glibc
+      xorg.libX11
+      xorg.libXext
+      xorg.libXrandr
+      xorg.libXtst
+      alsa-lib
+      libpulseaudio
+      libxkbcommon
+      libGL
+      qt5.qtbase
+      qt5.qttools
+      qt5.qtsvg
+
+      legacyPkgs.python37
+    ];
+  };
+}
