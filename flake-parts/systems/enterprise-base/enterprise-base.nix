ba9770f1ae48ce61c3e5f5a1d703ae3473fe95a7
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-16 13:41:51 +1000

    Update enterprise-base.nix

--- flake-parts/systems/enterprise-base/enterprise-base.nix
+++ flake-parts/systems/enterprise-base/enterprise-base.nix
@@ -4,7 +4,7 @@ let
   host = builtins.getEnv "SYSTEM_HOSTNAME";
 in
 {
-  flake.nixosConfigurations."${host} = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations."${host}" = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = { inherit inputs; };
 
