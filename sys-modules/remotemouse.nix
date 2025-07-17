556d04ca1c1fde5556f04d06cc54b4c4681200c6
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-17 18:56:24 +1000

    Update remotemouse.nix

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -1,10 +1,6 @@
 #/etc/nixos/sys-modules/remotemouse.nix
 { config, lib, pkgs, ... }:
 {
-  imports = [
-    inputs.self.nixosModules.remotemouse
-  ];
-
   networking.firewall = {
     allowedTCPPorts = [ 1978 ];
     allowedUDPPorts = [ 1978 ];
