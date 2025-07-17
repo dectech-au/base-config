61ca1c850cb293224ccfdd75a0326fea3dfaa077
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-17 18:55:20 +1000

    Update remotemouse.nix

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -1,37 +1,12 @@
-# ~/.dotfiles/modules/remotemouse.nix
-
-# bash 
-# cd ~ && sudo rm -r .wine ##### clean wine prefix and start again
-
-# wineserver -k                             ##### restart wine
-
-##### Install these libraries 
-# winetricks corefonts dinput8 directplay input8 nocrashdialog vcrun2015
-
-##### Libraries which breaks shit:
-# dotnet40 gdiplus
-
-##### Force Wine to handle keyboard and mouse input:
-# wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "UseTakeFocus" /d "Y" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "UseXVidMode" /d "N" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "GrabKeyboard" /d "Y" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "GrabPointer" /d "Y" /f && wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v "MouseWarpOverride" /d "force" /f
-
-##### Input device permissions:
-# sudo groupadd uinput && sudo usermod -aG input,uinput $(whoami) 
-
-##### Run remote mouse
-# cd ~/.wine/drive_c/Program\ Files\ \(x86\)/Remote\ Mouse/ && WINEDEBUG=+cursor,+event,+x11drv wine RemoteMouse.exe 
-
-
+#/etc/nixos/sys-modules/remotemouse.nix
 { config, lib, pkgs, ... }:
 {
+  imports = [
+    inputs.self.nixosModules.remotemouse
+  ];
 
   networking.firewall = {
     allowedTCPPorts = [ 1978 ];
     allowedUDPPorts = [ 1978 ];
   };
-
-  environment.systemPackages = with pkgs; [
-    evtest
-    winetricks
-    wineWowPackages.staging
-  ];
 }
