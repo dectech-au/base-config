45342f24d853be1a3f57130d5b47816101fd0620
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-17 16:23:01 +1000

    Create switch.sh

---
+++ hosts/enterprise-base/switch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+(
+   set -euo pipefail
+
+   cd /etc/nixos
+
+   eval "$(ssh-agent -s)"
+   ssh-add ~/.ssh/id_nixos_readonly
+   git fetch origin
+   git reset --hard origin/main
+
+   # Define the timestamp file
+   STAMP_FILE="/tmp/nix_flake_update.timestamp"
+
+   # Check if the file exists and if it's less than 10 minutes old
+   if [[ ! -f "$STAMP_FILE" || $(($(date +%s) - $(< "$STAMP_FILE"))) -ge 600 ]]; then
+    echo "Running nix flake update..."
+    nix flake update
+    date +%s > "$STAMP_FILE"
+   else
+    echo "Skipping nix flake update (ran recently)."
+   fi
+
+   sudo nixos-rebuild switch --upgrade --flake /etc/nixos/#enterprise-base --show-trace
+
+   echo "Done!"
+   sleep 2  # short pause before closing
+) && exit
