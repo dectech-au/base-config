0c52e03a6a74292782ed357de54cbab15fec866f
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-17 11:02:15 +1000

    Delete modules/star-rail.nix

--- modules/star-rail.nix
+++
@@ -1,8 +0,0 @@
-#~/.dotfiles/modules/star-rail.nix
-{ config, lib, pkgs, ... }:
-  let
-    aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
-  in
-{
-  home.packages = [ aagl-gtk-on-nix.the-honkers-railway-launcher ];
-}
