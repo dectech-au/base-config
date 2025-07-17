24dc73668f5f76161b2f942c5f59727ca36e989c
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-17 19:22:08 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,108 @@
-@@
-- , makeWrapper
-- , xdotool ? null  # optional; see note below
-+ , makeWrapper
-+ , patchelf
-+ , libX11
-+ , libXtst
-+ , libXi
-+ , alsa-lib
-+ , xdotool ? null
-@@
--in
--stdenv.mkDerivation rec {
-+  runtimeLibPath = lib.makeLibraryPath [
-+    libX11 libXtst libXi alsa-lib
-+    stdenv.cc.cc.lib
-+    stdenv.cc.libc
-+  ];
-+in stdenv.mkDerivation rec {
-@@
--  nativeBuildInputs = [ makeWrapper ];
-+  nativeBuildInputs = [ makeWrapper patchelf ];
-@@
--  dontPatchELF = true;  # vendor binary ships working RPATH relative to $ORIGIN
--  dontStrip = true;     # keep their binary intact
-+  postFixup = ''
-+    patchelf --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
-+             --set-rpath "$out/opt/remotemouse/lib:${runtimeLibPath}" \
-+             $out/opt/remotemouse/RemoteMouse || true
-+    for so in $out/opt/remotemouse/lib/*.so*; do
-+      [ -e "$so" ] || continue
-+      patchelf --set-rpath "$out/opt/remotemouse/lib:${runtimeLibPath}" "$so" || true
-+    done
-+  '';
-+  dontPatchELF = true;
-+  dontStrip = true;
+#~/.dotfiles/flake.nix
+
+{
+
+  description = "DecTec default flake";
+
+
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    #flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+
+
+
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+
+
+    nixvim.url = "github:nix-community/nixvim";
+
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+
+
+
+    aagl.url = "github:ezKEa/aagl-gtk-on-nix/release-25.05";
+
+
+    aagl.inputs.nixpkgs.follows = "nixpkgs";
+
+
+
+
+  };
+
+
+
+  outputs = inputs@{ self, flake-parts, ... }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [ "x86_64-linux" ];
+
+      imports = [
+
+        ./flake-parts/overlays.nix
+
+        ./flake-parts/systems/enterprise-base/enterprise-base.nix
+
+        ./flake-parts/systems/personal-tim/personal-tim.nix
+
+      ];
+
+
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        formatter = pkgs.nixpkgs-fmt;
+
+
+
+        packages = {
+
+          remotemouse = pkgs.callPackage ./remotemouse {
+
+            xdotool = pkgs.xdotool; # drop if unneeded
+
+          };
+
+          default = self'.packages.remotemouse;
+
+        };
+
+
+
+        devShells.default = pkgs.mkShell {
+
+          packages = with pkgs; [
+
+            git
+
+            nixpkgs-fmt
+
+            neovim
+
+          ];
+
+          shellHook = ''
+
+            echo "Welcome to the mutha' fuckin' dev shell, you stupid bitch."
+
+          '';
+
+        };
+
+      };
+
+    };
+
+}
