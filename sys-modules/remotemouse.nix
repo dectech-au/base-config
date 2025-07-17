1bb17703022eed3094b14e0d3f5145f62ae7231c
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-17 20:45:50 +1000

    Update remotemouse.nix

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -8,7 +8,7 @@ in {
     rmPkg
     pkgs.xorg.xhost  # handy for X auth troubleshooting
   ];
-
+  nixpkgs.config.allowUnfree = true;
   networking.firewall.allowedTCPPorts = [ 1978 ];
   networking.firewall.allowedUDPPorts = [ 1978 ];
 }
