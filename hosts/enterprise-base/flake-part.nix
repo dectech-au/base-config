1955d39575c67daee61924d141bc46092835cae2
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-19 19:12:51 +1000

    Create flake-part.nix

---
+++ hosts/enterprise-base/flake-part.nix
@@ -0,0 +1,22 @@
+#/etc/nixos/hosts/enterprise-base/flake-part.nix
+{ inputs, ... }:
+{
+  flake.nixosConfigurations.enterprise-base = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
+
+    modules = [
+      { nixpkgs.config.allowUnfree = true; }
+      ../../../hosts/enterprise-base/configuration.nix
+      inputs.home-manager.nixosModules.home-manager {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          backupFileExtension = "backup";
+          users.dectec = import ../../../hosts/enterprise-base/home.nix;
+        };
+      }
+      inputs.nixvim.nixosModules.nixvim
+    ];
+  };
+}
