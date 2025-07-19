cea2c7296bd378b5ff6b25c7d4caa44fc2ba0001
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-20 09:54:09 +1000

    Update configuration.nix

--- hosts/personal-tim/configuration.nix
+++ hosts/personal-tim/configuration.nix
@@ -41,6 +41,7 @@
     ../../sys-modules/remotemouse.nix
     ../../sys-modules/signal-desktop.nix
     ../../sys-modules/sshfs.nix
+		../../sys-modules/star-rail-macro.nix
     ../../sys-modules/steam.nix
     ../../sys-modules/tailscale.nix
     ../../sys-modules/teams.nix
