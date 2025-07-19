6fa1194e733a77e0f8f941ca870db279af4e1830
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-19 20:54:31 +1000

    Update windows.nix

--- home-modules/start-menu/windows.nix
+++ home-modules/start-menu/windows.nix
@@ -4,7 +4,7 @@
   xdg.desktopEntries.Windows = {
     name = "Windows";
     comment = "Restart to Windows";
-    exec = "kitty -e sudo systemctl reboot --boot-loader-entry=auto-windows";
+    exec = "sudo ${pkgs.systemd}/bin/systemctl reboot --boot-loader-entry=auto-windows";
     icon = "distributor-logo-windows";
     terminal = true;
     type = "Application";
