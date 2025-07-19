ce0adb02dab1e8f9f79dced3f6e926dd92709631
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-19 10:13:24 +1000

    Update configuration.nix

--- hosts/personal-tim/configuration.nix
+++ hosts/personal-tim/configuration.nix
@@ -2,7 +2,7 @@
 { config, lib, pkgs, ... }:
 let
 	host = lib.strings.removeSuffix "\n"
-		(builtins.readFile /etc/nixos/system-hostname.txt);
+		(builtins.readFile ../../system-hostname.txt);
 in {
   imports = [ 
     ../../hardware-configuration.nix
