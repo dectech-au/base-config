c1c90ede1b3f109d62a585145003fa9d244948df
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-20 13:48:26 +1000

    Update emacs.nix

--- sys-modules/emacs.nix
+++ sys-modules/emacs.nix
@@ -5,52 +5,54 @@
     enable = true;
     package = pkgs.emacsPgtk;
 
-  # Emacs build packages
-  extraPackages = epkgs: with epkgs; [
-    evil             # Vim-style modal editing
-    which-key        # popup cheat-sheet for keybindings
-    magit            # best Git UI on the planet
-    use-package      # declarative package/config loader
-    org              # Org-mode
-    nix-mode         # syntax + indentation for .nix files
+    # Emacs build packages
+    extraPackages = epkgs: with epkgs; [
+      evil             # Vim-style modal editing
+      which-key        # popup cheat-sheet for keybindings
+      magit            # best Git UI on the planet
+      use-package      # declarative package/config loader
+      org              # Org-mode
+      nix-mode         # syntax + indentation for .nix files
+    ];
 
-  # Core init file (loads before optional ~/.emacs.d files)
-  extraConfig = ''
-    ;; ==== UI cleanup ====
-    (menu-bar-mode      -1)
-    (tool-bar-mode      -1)
-    (scroll-bar-mode    -1)
-    (blink-cursor-mode  0)
-    (setq inhibit-startup-screen t)
-    (setq ring-bell-function 'ignore)
-
-    ;; ==== Line numbers + column ====
-    (global-display-line-numbers-mode 1)
-    (column-number-mode 1)
-
-    ;; ==== Evil (Vim) ====
-    (setq evil-want-keybinding nil) ; avoid old compatibility layer
-    (use-package evil
-      :config
-      (evil-mode 1))
-
-    ;; ==== Which-key ====
-    (use-package which-key
-      :diminish
-      :config
-      (which-key-mode 1))
-
-    ;; ==== Magit ====
-    (use-package magit
-      :commands (magit-status))
-
-    ;; ==== Org ====
-    (use-package org
-      :hook ((org-mode . visual-line-mode))
-      :config
-      (setq org-hide-emphasis-markers t
-            org-startup-indented t))
-
-    ;; ==== Files end here ====
-  '';
-};
+    # Core init file (loads before optional ~/.emacs.d files)
+    extraConfig = ''
+      ;; ==== UI cleanup ====
+      (menu-bar-mode      -1)
+      (tool-bar-mode      -1)
+      (scroll-bar-mode    -1)
+      (blink-cursor-mode  0)
+      (setq inhibit-startup-screen t)
+      (setq ring-bell-function 'ignore)
+  
+      ;; ==== Line numbers + column ====
+      (global-display-line-numbers-mode 1)
+      (column-number-mode 1)
+  
+      ;; ==== Evil (Vim) ====
+      (setq evil-want-keybinding nil) ; avoid old compatibility layer
+      (use-package evil
+        :config
+        (evil-mode 1))
+  
+      ;; ==== Which-key ====
+      (use-package which-key
+        :diminish
+        :config
+        (which-key-mode 1))
+  
+      ;; ==== Magit ====
+      (use-package magit
+        :commands (magit-status))
+  
+      ;; ==== Org ====
+      (use-package org
+        :hook ((org-mode . visual-line-mode))
+        :config
+        (setq org-hide-emphasis-markers t
+              org-startup-indented t))
+  
+      ;; ==== Files end here ====
+    '';
+  };
+}
