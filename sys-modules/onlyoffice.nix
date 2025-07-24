37c3d76fe2d7380a4bb0a49c9824103f6594b9f0
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-24 18:46:07 +1000

    Update onlyoffice.nix

--- sys-modules/onlyoffice.nix
+++ sys-modules/onlyoffice.nix
@@ -1,20 +1,8 @@
 #/etc/nixos/sys-modules/onlyoffice.nix
 { config, lib, pkgs, ... }:
-let
-  onlyofficeEnv = pkgs.runFHSUserEnv {
-    name = "onlyoffice-with-fonts";
-    targetPkgs = pkgs: with pkgs; [
-      onlyoffice-bin
-      cantarell-fonts
-      # add any other font packages you need here
-    ];
-    multiPkgs = true;
-    runScript = "desktopeditors";  # or whatever the onlyoffice entrypoint is
-  };
-in {
+{
 	environment.systemPackages = with pkgs; [
 		onlyoffice-bin
- 		onlyofficeEnv
 	];
 
 	nixpkgs.config.allowUnfreePredicate = pkg:
