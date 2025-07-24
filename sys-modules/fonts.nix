c5c54f4f4cd8aa0f90c5a5c221baf4dccddc9280
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-24 17:50:03 +1000

    Update fonts.nix

--- sys-modules/fonts.nix
+++ sys-modules/fonts.nix
@@ -1,8 +1,8 @@
-#/etc/nixos/modules/fonts.nix
+#/etc/nixos/sys-modules/fonts.nix
 { config, lib, pkgs, ... }:
 {
 	fonts.packages = with pkgs; [
 		corefonts
-    cantarell-fonts
+		cantarell-fonts
 	];
 }
