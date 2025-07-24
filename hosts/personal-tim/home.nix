0f5af40d4e1db9fad31d451dd9c34eba1cd2b3f1
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-24 19:05:26 +1000

    Update home.nix

--- hosts/personal-tim/home.nix
+++ hosts/personal-tim/home.nix
@@ -11,7 +11,7 @@
 		../../home-modules/kdeconnect.nix
 		../../home-modules/kitty.nix
 		../../home-modules/librewolf.nix
-		../../home-modules/onlyoffice.nix
+		#../../home-modules/onlyoffice.nix
 		#../../home-modules/papirus-theme.nix
 		#../../home-modules/remotemouse.nix
 		../../home-modules/thunderbird.nix
