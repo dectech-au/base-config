402508f231913832230897fe28091dc9ce902b86
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-24 19:06:47 +1000

    Update onlyoffice.nix

--- sys-modules/onlyoffice.nix
+++ sys-modules/onlyoffice.nix
@@ -1,31 +1,46 @@
 #/etc/nixos/sys-modules/onlyoffice.nix
 { config, lib, pkgs, ... }:
+let
+  onlyofficeEnv = pkgs.buildFHSEnv {
+    name       = "onlyoffice";
+    targetPkgs = pkgs: with pkgs; [
+      onlyoffice-bin
+      cantarell-fonts
+    ];
+    runScript  = "desktopeditors";
+  };
+in
 {
-	environment.systemPackages = with pkgs; [
-		onlyoffice-bin
-	];
+  environment.systemPackages = [ onlyofficeEnv ];
+}	
 
-	nixpkgs.config.allowUnfreePredicate = pkg:
-		builtins.elem (lib.getName pkg)
-		[
-			"corefonts"
-			"cantarell-fonts"
-		];
 
-	fonts = {
+
+#	environment.systemPackages = with pkgs; [
+#		onlyoffice-bin
+#	];
+
+#	nixpkgs.config.allowUnfreePredicate = pkg:
+#		builtins.elem (lib.getName pkg)
+#		[
+#			"corefonts"
+#			"cantarell-fonts"
+#		];
+
+#	fonts = {
 		
-		enableDefaultPackages = true;
-		enableGhostscriptFonts = true;
+#		enableDefaultPackages = true;
+#		enableGhostscriptFonts = true;
 
-		packages = with pkgs; [
-			corefonts
-			vista-fonts
-			liberation_ttf
-			cantarell-fonts
-			dejavu_fonts
-			noto-fonts
-			fira-code
-			jetbrains-mono
-		];
-	};
-}
+#		packages = with pkgs; [
+#			corefonts
+#			vista-fonts
+#			liberation_ttf
+#			cantarell-fonts
+#			dejavu_fonts
+#			noto-fonts
+#			fira-code
+#			jetbrains-mono
+#		];
+#	};
+#}
