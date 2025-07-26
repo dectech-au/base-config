91741225f2aaddea7799b7bfae4afe3ca4e72d0a
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-26 13:15:36 +1000

    Update hostname.nix

--- sys-modules/hostname.nix
+++ sys-modules/hostname.nix
@@ -2,6 +2,8 @@
 { config, lib, pkgs, ... }:
 
 {
+  services.systemd-hostnamed.enable = false;
+  
   system.activationScripts.generateHostName.text = ''
     serial=$(tr -d ' ' </sys/class/dmi/id/product_serial 2>/dev/null)
     if [ -z "$serial" ] || [ "$serial" = "Unknown" ]; then
