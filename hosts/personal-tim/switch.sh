86afb159cab557c53c451e80255fdde972b97f0c
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-26 11:59:35 +1000

    Update switch.sh

--- hosts/personal-tim/switch.sh
+++ hosts/personal-tim/switch.sh
@@ -13,24 +13,6 @@ git reset --hard origin/main
 # ── Build /etc/nixos/system-hostname.txt every run ──────────────────────
 sudo bash /etc/nixos/scripts/update-hostname.sh
 
-
-#serial=$(sudo cat /sys/class/dmi/id/product_serial 2>/dev/null | tr -d ' ')
-#[[ -z $serial || $serial == "Unknown" ]] && serial=$(cut -c1-8 /etc/machine-id)
-#hostname="dectech-${serial: -6}"
-
-#file=/etc/nixos/hosts/system-hostname.txt
-#if [[ ! -f $file || $(<"$file") != "$hostname" ]]; then
-#  echo "Updating hostname file → $hostname"
-#  echo "$hostname" | sudo tee "$file" >/dev/null
-#fi
-
-#cat <<EOF | sudo tee /etc/nixos/hosts/hostname.nix >/dev/null
-#{ config, lib, pkgs, ... }:
-#{
-#  networking.hostName = "${hostname}";
-#}
-#EOF
-
 # ── Throttle nix flake update to once per 10 min ────────────────────────
 stamp=/tmp/nix_flake_update.timestamp
 if [[ ! -f $stamp || $(( $(date +%s) - $(<"$stamp") )) -ge 600 ]]; then
