663be285b042e155225584c3173f0ce626d83d11
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-26 11:34:25 +1000

    Update hostname.nix

--- sys-modules/hostname.nix
+++ sys-modules/hostname.nix
@@ -1,24 +1,5 @@
 #/etc/nixos/sys-modules/hostname.nix
 { config, lib, pkgs, ... }:
-
-let
-  hostScript = ''
-    #!/usr/bin/env bash
-
-    serial=$(cat /sys/class/dmi/id/product_serial 2>/dev/null | tr -d ' ')
-    if [ -z "$serial" ] || [ "$serial" = "Unknown" ]; then
-      serial=$(cut -c1-8 /etc/machine-id)
-    fi
-
-    name="dectech-''${serial: -6}"
-
-    if [ "$(cat /proc/sys/kernel/hostname)" != "$name" ]; then
-      echo "setting hostname to $name"
-      echo "$name" > /etc/hostname
-      hostname "$name"
-    fi
-  '';
-in
 {
 system.activationScripts.generateHostName.text = ''
   serial=$(tr -d ' ' </sys/class/dmi/id/product_serial 2>/dev/null)
@@ -30,11 +11,4 @@ system.activationScripts.generateHostName.text = ''
     hostname "$name"        # kernel only – no file write
   fi
 ''
-
-
-  # placeholder so eval-time modules have *some* value
-  # networking.hostName = lib.mkDefault "dectech-placeholder";
-
-  # run the bash snippet every switch / boot
-  # system.activationScripts.generateHostName.text = hostScript;
 }
