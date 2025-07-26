61d2851cb3ab64cade998372442139ede6bcd22f
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-26 11:41:49 +1000

    Update hostname.nix

--- sys-modules/hostname.nix
+++ sys-modules/hostname.nix
@@ -1,14 +1,13 @@
 #/etc/nixos/sys-modules/hostname.nix
 { config, lib, pkgs, ... }:
-
 {
   system.activationScripts.generateHostName.text = ''
     serial=$(tr -d ' ' </sys/class/dmi/id/product_serial 2>/dev/null)
     [ -z "$serial" ] || [ "$serial" = "Unknown" ] && serial=$(cut -c1-8 /etc/machine-id)
-
-    # Escape the $ so Nix leaves it alone
-    name="dectech-$${serial: -6}"
-
+  
+    suffix=$(echo "$serial" | tail -c 7)
+    name="dectech-$suffix"
+  
     if [ "$(cat /proc/sys/kernel/hostname)" != "$name" ]; then
       echo "setting hostname to $name"
       hostname "$name"
