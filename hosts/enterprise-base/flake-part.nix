2cbcd1bd790f96341d32f74d3159839468c733bc
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-29 10:43:56 +1000

    Update flake-part.nix

--- hosts/enterprise-base/flake-part.nix
+++ hosts/enterprise-base/flake-part.nix
@@ -1,22 +1,27 @@
 #/etc/nixos/hosts/enterprise-base/flake-part.nix
-{ inputs, ... }:
+{ inputs, self, ... }:
 {
+  systems = [ "x86_64-linux" ];
+
   flake.nixosConfigurations.enterprise-base = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = { inherit inputs; };
 
     modules = [
       { nixpkgs.config.allowUnfree = true; }
-      ../../../hosts/enterprise-base/configuration.nix
-      inputs.home-manager.nixosModules.home-manager {
+      ../../sys-modules/hostname.nix
+      ./configuration.nix
+      inputs.nixvim.nixosModules.nixvim
+      inputs.home-manager.nixosModules.home-manager
+      {
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
           backupFileExtension = "backup";
-          users.dectec = import ../../../hosts/enterprise-base/home.nix;
+          users.dectec = import ./home.nix;
+          sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
         };
       }
-      inputs.nixvim.nixosModules.nixvim
     ];
   };
 }
