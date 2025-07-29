17e67c4cdeb543fb7181076ad75c53a2e8b0f37c
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-29 16:21:53 +1000

    Update plasma-manager.nix

--- home-modules/plasma-manager.nix
+++ home-modules/plasma-manager.nix
@@ -38,12 +38,8 @@
       baloofilerc."Basic Settings"."Indexing-Enabled" = false;
       kwinrc."org.kde.kdecoration2".ButtonsOnLeft = "SF";
 
-      "kactivitymanagerdrc".groups."Favorites".values = {
-        favorites = lib.concatStringsSep "," [
-          "systemsettings.desktop"
-          "org.kde.dolphin.desktop"
-          "firefox.desktop"
-        ];
+      "kactivitymanagerdrc".groups."Favorites".favorites =
+        "systemsettings.desktop,org.kde.dolphin.desktop,firefox.desktop";
       };
 
       kwinrc.Desktops.Number = {
