0fe5f57dbcf31b01498ade9797c9b9fc98bb618f
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-29 18:11:22 +1000

    Update onlyoffice.nix

--- sys-modules/onlyoffice.nix
+++ sys-modules/onlyoffice.nix
@@ -1,24 +1,9 @@
 #/etc/nixos/sys-modules/onlyoffice.nix
 { config, lib, pkgs, ... }:
-let
-  onlyofficeEnv = pkgs.buildFHSEnv {
-    name       = "onlyoffice";
-    targetPkgs = pkgs: with pkgs; [
-      onlyoffice-bin
-      cantarell-fonts
-    ];
-    runScript  = "desktopeditors";
-  };
-in
 {
-  environment.systemPackages = [ onlyofficeEnv ];
-}	
-
-
-
-#	environment.systemPackages = with pkgs; [
-#		onlyoffice-bin
-#	];
+	environment.systemPackages = with pkgs; [
+		onlyoffice-bin
+	];
 
 #	nixpkgs.config.allowUnfreePredicate = pkg:
 #		builtins.elem (lib.getName pkg)
