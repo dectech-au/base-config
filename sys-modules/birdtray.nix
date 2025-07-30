30edaec95deed1dc05a0d6f1c61d2d4623e92e34
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-30 07:52:28 +0000

    Update birdtray.nix

--- sys-modules/birdtray.nix
+++ sys-modules/birdtray.nix
@@ -1,7 +1,14 @@
 #~/.dotfiles/modules/birdtray.nix
 { config, lib, pkgs, ... }:
+let
+  myBirdtray = pkgs.birdtray.overrideAttrs (old: {
+    cmakeFlags = (old.cmakeFlags or []) ++ [
+      "-DOPT_THUNDERBIRD_CMDLINE=${pkgs.thunderbird}/bin/thunderbird"
+    ];
+  });
+in
 {
   environment.systemPackages = with pkgs; [
-    birdtray
+    myBirdtray
   ];
 }
