4304c37eb940d9da155d410d11559dfa9e745128
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-02 05:18:39 +0000

    Update tailscale.nix

--- sys-modules/tailscale.nix
+++ sys-modules/tailscale.nix
@@ -6,9 +6,9 @@ let
 in
 {
   # Ensure headscale.dectech.au resolves to your SWAG/LAN IP
-  networking.hosts = lib.mkForce {
-    "headscale.dectech.au" = [ "192.168.1.157" ];
-  };
+  # networking.hosts = lib.mkForce {
+  #  "headscale.dectech.au" = [ "192.168.1.157" ];
+  # };
 
   # copy the file from your home dir into /etc/tailscale/hskey.txt
   environment.etc."tailscale/hskey.txt".source = "/home/dectec/.secrets/hskey.txt";
