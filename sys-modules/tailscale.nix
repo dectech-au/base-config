3c4adae173a93adbd4c23df6f2a78d392b1b8f8c
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-02 02:33:49 +0000

    Update tailscale.nix

--- sys-modules/tailscale.nix
+++ sys-modules/tailscale.nix
@@ -1,8 +1,26 @@
 #/etc/nixos/sys-modules/tailscale.nix
-{ config, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+let
+  # Path inside the Nix store (or under /etc) where you’ll stash your Headscale API key
+  hsKeyPath = "/etc/tailscale/hskey.txt";
+in
 {
+  # Drop your key into /etc via Nix (or copy it there by hand)
+  environment.etc."tailscale/hskey.txt".source = ./secrets/hskey.txt;
+
   services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "client";
+    enable              = true;
+    useRoutingFeatures  = "client";
+
+    # point tailscaled at your Headscale mesh
+    authKeyFile         = hsKeyPath;                                             # :contentReference[oaicite:0]{index=0}
+    extraUpFlags        = [
+      "--login-server=https://headscale.dectech.au"                              # :contentReference[oaicite:1]{index=1}
+      "--accept-dns=true"       # let Headscale DNS (MagicDNS + overrides) work
+    ];
+
+    # if you want tailscale to punch holes in your firewall
+    openFirewall         = true;
   };
 }
