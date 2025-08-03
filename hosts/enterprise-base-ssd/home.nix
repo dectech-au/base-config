c7da82900f10d7880cf45350e73058f3c9a66365
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-03 10:00:26 +1000

    Create home.nix

---
+++ hosts/enterprise-base-ssd/home.nix
@@ -0,0 +1,43 @@
+#/etc/nixos/hosts/enterprise-base-ssd/home.nix
+{ config, pkgs, HOME-MODULES, ... }:
+{
+  imports = [
+		../../home-modules/dropbox.nix
+		#../../home-modules/emacs.nix
+		../../home-modules/fastfetch.nix
+		#../../home-modules/firefox.nix # Moved to configuration.nix
+		../../home-modules/fish-enterprise-base.nix
+		../../home-modules/git.nix
+		../../home-modules/kdeconnect.nix
+		../../home-modules/kitty.nix
+		../../home-modules/librewolf.nix
+		#../../home-modules/onlyoffice.nix
+		#../../home-modules/papirus-theme.nix
+		../../home-modules/plasma-manager.nix
+		#../../home-modules/remotemouse.nix
+		../../home-modules/thunderbird.nix
+		#../../home-modules/thunderbird-theme.nix
+		../../home-modules/vscode.nix
+		../../home-modules/yt-dlp.nix
+		#../../home-modules/start-menu/onlyoffice.nix
+		../../home-modules/start-menu/teams.nix
+		../../home-modules/start-menu/update.nix
+		../../home-modules/start-menu/windows.nix
+		#./personalisation/wallpaper.nix
+  ];
+
+  home.username = "dectec";
+  home.homeDirectory = "/home/dectec";
+  home.stateVersion = "25.05"; # Do not change this after initial setup
+
+
+  programs.git = {
+    enable = true;
+    userName = "DecTec";
+    userEmail = "zozano@protonmail.com";
+  };
+
+  # home.file.".local/share/applications/teams.desktop".text = ''
+  #   test
+  # '';
+}
