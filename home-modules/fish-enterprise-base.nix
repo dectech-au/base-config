90d53c1d43d8b60a65997524914635099b499d70
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-04 23:28:44 +0000

    Update fish-enterprise-base.nix

--- home-modules/fish-enterprise-base.nix
+++ home-modules/fish-enterprise-base.nix
@@ -7,6 +7,7 @@
     shellInitLast = "fastfetch";
     shellAbbrs = {
       "update" = "sudo bash /etc/nixos/hosts/enterprise-base/switch.sh";
+      "server" = "ssh -t z-home@192.168.1.157 'tmux attach -t main || tmux new -s main fish'"
     };
   };
 
