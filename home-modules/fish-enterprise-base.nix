745c2417b986a33fb5890975eeff42b0b6e8c00e
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-04 23:41:39 +0000

    Update fish-enterprise-base.nix

--- home-modules/fish-enterprise-base.nix
+++ home-modules/fish-enterprise-base.nix
@@ -7,8 +7,7 @@
     shellInitLast = "fastfetch";
     shellAbbrs = {
       "update" = "sudo bash /etc/nixos/hosts/enterprise-base/switch.sh";
-      "server" = "ssh -t z-home@192.168.1.157 \"tmux attach -t main || tmux new -s main fish\"";
-    };
+      server = "ssh -t z-home@192.168.1.157 'tmux attach -t main || tmux new -s main fish'";
   };
 
   home.file.".config/fish/conf.d/disable-greeting.fish".text = ''
