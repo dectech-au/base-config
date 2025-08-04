090aedb5bf461cc70ccd747a88ae60be438bb1a6
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-04 03:23:47 +0000

    Update prometheus.nix

--- sys-modules/prometheus.nix
+++ sys-modules/prometheus.nix
@@ -7,6 +7,5 @@
     port          = 9100;
     openFirewall  = true;
   };
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
 }
 # Don't forget to add target's tailscale address to prometheus on the admin pc
