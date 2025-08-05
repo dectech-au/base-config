dece09e9b1c5b983cdb5c2569af68ba3e1f0561c
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-05 10:12:50 +0000

    Update sys-weekly-schedule.nix

--- custom-modules/hastings-preschool/sys-weekly-schedule.nix
+++ custom-modules/hastings-preschool/sys-weekly-schedule.nix
@@ -2,10 +2,11 @@
 { pkgs, ... }:
 
 let
-  weeklyScheduleEnv = pkgs.python311.withPackages (ps: [
-    ps.pdfplumber
-    ps.openpyxl
-  ]);
+weeklyScheduleEnv = pkgs.python311.withPackages (ps: [
+  ps.pdfplumber.overridePythonAttrs (_: { doCheck = false; })
+  ps.openpyxl.overridePythonAttrs    (_: { doCheck = false; })
+]);
+
 in
 {
   environment.systemPackages = [ weeklyScheduleEnv ];
