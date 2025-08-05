34cae7d705fc3cabb2da3ca7c43b95cb7135d080
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-05 10:51:09 +0000

    Create home.nix

---
+++ hosts/ G531GT-AL017T/home.nix
@@ -0,0 +1,44 @@
+#/etc/nixos/hosts/G531GT-AL017T/home.nix
+{ config, pkgs, HOME-MODULES, ... }:
+{
+  imports = [
+		../../home-modules/dropbox.nix
+		#../../home-modules/emacs.nix
+		../../home-modules/fastfetch.nix
+		#../../home-modules/firefox.nix # Moved to configuration.nix
+		../../home-modules/fish-G531GT-AL017T.nix
+		../../home-modules/git.nix
+		../../home-modules/kdeconnect.nix
+		../../home-modules/kitty.nix
+		../../home-modules/librewolf.nix
+		#../../home-modules/onlyoffice.nix
+		#../../home-modules/papirus-theme.nix
+		../../home-modules/plasma-manager.nix
+		#../../home-modules/remotemouse.nix
+		../../home-modules/thunderbird.nix
+		#../../home-modules/thunderbird-theme.nix
+		../../home-modules/vscode.nix
+		../../home-modules/yt-dlp.nix
+		#../../home-modules/start-menu/onlyoffice.nix
+		../../home-modules/start-menu/teams.nix
+		../../home-modules/start-menu/update.nix
+		../../home-modules/start-menu/windows.nix
+		#./personalisation/wallpaper.nix
+		../../custom-modules/hastings-preschool/home-weekly-schedule.nix
+  ];
+
+  home.username = "dectec";
+  home.homeDirectory = "/home/dectec";
+  home.stateVersion = "25.05"; # Do not change this after initial setup
+
+
+  programs.git = {
+    enable = true;
+    userName = "DecTec";
+    userEmail = "zozano@protonmail.com";
+  };
+
+  # home.file.".local/share/applications/teams.desktop".text = ''
+  #   test
+  # '';
+}
