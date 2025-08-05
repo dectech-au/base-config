971d2be6da7c3e8ce5d147d242d4150169497097
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-05 21:39:11 +1000

    Update flake-part.nix

--- hosts/G531GT-AL017T/flake-part.nix
+++ hosts/G531GT-AL017T/flake-part.nix
@@ -1,28 +1,50 @@
-#/etc/nixos/hosts/G531GT-AL017T/flake-part.nix
-{ inputs, self, ... }:
-{
+# /etc/nixos/hosts/G531GT-AL017T/flake-part.nix
+#
+# Accept an argument `hostName` (defaults to "placeholder").
+# Pass it through `specialArgs`, then set networking.hostName
+# in an inline module.  No generated file is needed.
+
+{ inputs
+, self
+, hostName ? "placeholder"  # will be overridden from the CLI
+, ...
+}: {
   systems = [ "x86_64-linux" ];
 
-  flake.nixosConfigurations.G531GT-AL017T = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    specialArgs = { inherit inputs; };
+  flake.nixosConfigurations.G531GT-AL017T =
+    inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
 
-    modules = [
-      { nixpkgs.config.allowUnfree = true; }
-      ../../sys-modules/hostname.nix
-      ./configuration.nix
-      inputs.nixvim.nixosModules.nixvim
-      inputs.remotemouse.nixosModules.remotemouse
-      inputs.home-manager.nixosModules.home-manager
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          backupFileExtension = "bak";
-          users.dectec = import ./home.nix;
-          sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
-        };
-      }
-    ];
-  };
+      # Make hostName available to every module
+      specialArgs = {
+        inherit inputs hostName;
+      };
+
+      modules = [
+        # Allow unfree packages
+        { nixpkgs.config.allowUnfree = true; }
+
+        # Inline module that sets the hostname
+        ({ config, hostName, ... }: {
+          networking.hostName = hostName;
+        })
+
+        # The rest of your configuration
+        ./configuration.nix
+        inputs.nixvim.nixosModules.nixvim
+        inputs.remotemouse.nixosModules.remotemouse
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs      = true;
+            useUserPackages    = true;
+            backupFileExtension = "bak";
+            users.dectec       = import ./home.nix;
+            sharedModules      = [
+              inputs.plasma-manager.homeManagerModules.plasma-manager
+            ];
+          };
+        }
+      ];
+    };
 }
