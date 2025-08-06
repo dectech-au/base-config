f5b0fb41cdfd8a866273ccccf3f7a884e2a6bdb4
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-06 10:29:24 +1000

    Update home-weekly-schedule.nix

--- custom-modules/hastings-preschool/home-weekly-schedule.nix
+++ custom-modules/hastings-preschool/home-weekly-schedule.nix
@@ -26,7 +26,6 @@ Actions=ConvertWeekly
 [Desktop Action ConvertWeekly]
 Name=Convert to Spreadsheet
 Icon=application-vnd.ms-excel
-Exec=/home/dectec/.local/bin/weekly-booking.py %f
-
+Exec=/home/dectec/.local/bin/weekly-booking.py "%f"
   '';
 }
