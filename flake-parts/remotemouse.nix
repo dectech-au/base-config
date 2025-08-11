06e18ab808339484099cd1429bf394f6b8873bc6
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 11:34:27 +1000

    Update remotemouse.nix

--- flake-parts/remotemouse.nix
+++ flake-parts/remotemouse.nix
@@ -1,46 +1,22 @@
 # flake-parts/remotemouse.nix
 { inputs, ... }:
-
 {
   systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
-  ##################################################
-  # 1.  Build the package and publish an overlay
-  ##################################################
+  
   perSystem = { pkgs, system, ... }:
     let
-      remotemouse =
-        pkgs.callPackage ./pkg/remote-mouse.nix {
-          xdotool = pkgs.xdotool;
-        };
-
+      remotemouse = pkgs.callPackage ./pkg/remote-mouse.nix { xdotool = pkgs.xdotool; };
       overlay = final: prev: { remotemouse = remotemouse; };
     in {
-      packages = {
-        inherit remotemouse;
-        default = remotemouse;
-      };
+      packages = { inherit remotemouse; default = remotemouse; };
       inherit overlay;
     };
 
-  ##################################################
-  # 2.  NixOS module that wires it up (merged config)
-  ##################################################
-  nixosModules.remotemouse = { lib, pkgs, ... }: {
+  flakeModules.remotemouse = { lib, pkgs, ... }: {
     config = {
-      # Pull in the overlay we just exported
-      nixpkgs.overlays = [
-        inputs.self.overlays.default
-      ];
-
-      # All the configuration from your sys-modules/remotemouse.nix
-      environment.systemPackages = [
-        pkgs.remotemouse
-        pkgs.xorg.xhost  # handy for X auth troubleshooting
-      ];
-      
+      nixpkgs.overlays = [ inputs.self.overlays.default ];
+      environment.systemPackages = [ pkgs.remotemouse pkgs.xorg.xhost ];
       nixpkgs.config.allowUnfree = true;
-      
       networking.firewall.allowedTCPPorts = [ 1978 ];
       networking.firewall.allowedUDPPorts = [ 1978 ];
     };
