87adcaac17519e41e6f4a71d26c2e802eb18f651
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 19:12:35 +1000

    Update sops.nix

--- sys-modules/sops.nix
+++ sys-modules/sops.nix
@@ -13,5 +13,18 @@
 }
 
 # process to set up sops
-# 1. generate key-pair with age
-# $ mkdir -p ~/.config/sops/age && age-keygen -o ~/.config/sops/age/keys.txt
+# 1. Generate age-key for admins
+# age-keygen -o ~/.config/sops/age/keys.txt
+# 2. create in the repo root ".sops.yaml" which contains the creation rules in the following format:
+# creation_rules:
+#    - path_regex: ^secrets/.*\.yaml$
+#      age:
+#        - age1<string>
+#
+# 3. generate the target machines public ssh key:
+# sudo ssh-keygen -A
+#
+# 4. Use ssh-to-age to generate the key
+# ssh-to-age < /etc/ssh/ssh_host_ed25519_key.pub
+#
+# 5. 
