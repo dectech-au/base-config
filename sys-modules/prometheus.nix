37869b59ff4f1cc9a49fccdad8dee4e91ed7b89e
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-12 09:57:15 +1000

    Update prometheus.nix

--- sys-modules/prometheus.nix
+++ sys-modules/prometheus.nix
@@ -3,9 +3,9 @@
 {
   services.prometheus.exporters.node = {
     enable        = true;
-    listenAddress = "0.0.0.0";
+    listenAddress = "127.0.0.1";
     port          = 9100;
-    openFirewall  = true;
+    #openFirewall  = true;
   };
 }
 # Don't forget to add target's tailscale address to prometheus on the admin pc
