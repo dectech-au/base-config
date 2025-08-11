0c82d42975759045a17447f4b9626be83fb4c159
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 12:52:18 +1000

    Update configuration.nix

--- hosts/G531GT-AL017T/configuration.nix
+++ hosts/G531GT-AL017T/configuration.nix
@@ -7,6 +7,7 @@
 	imports = [ 
 		#./hostname.nix
 		../../hardware-configuration.nix
+		../../sys-modules/age.nix
 		../../sys-modules/baobab.nix
 		../../sys-modules/birdtray.nix
 		../../sys-modules/bluetooth.nix
