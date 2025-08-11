23b98f9f30f47ee5cd0a6c1df8b1111b75b85694
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 15:10:00 +1000

    Update sops.nix

--- sys-modules/sops.nix
+++ sys-modules/sops.nix
@@ -1,7 +1,7 @@
 # sys-modules/sops.nix
 { pkgs, sops-nix, ... }:
 let
-  ageKeyFile = pkgs.writeText "age-key" (builtins.readFile ../../sops-age-key.txt);
+  ageKeyFile = pkgs.writeText "age-key" (builtins.readFile /var/lib/sops-nix/key.txt);
 in
 {
   imports = [ sops-nix.nixosModules.sops ];
