c9fbe66ed44d4118ab809d1f7f30da4ad197a5fd
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 11:39:10 +1000

    Update sops.nix

--- flake-parts/sops.nix
+++ flake-parts/sops.nix
@@ -38,4 +38,22 @@
         '';
       };
     };
+
+  flakeModules.sops = { lib, pkgs, ... }: {
+    config = {
+      nixpkgs.overlays = [ inputs.self.overlays.default ];
+      imports = [ inputs.sops-nix.nixosModules.sops ];
+      sops = {
+        defaultSopsFile = ./secrets.yaml;
+        age.keyFile = "/var/lib/sops-nix/key.txt";
+        secrets = {
+          "ssh_host_rsa_key" = { mode = "0400"; owner = "root"; group = "root"; };
+          "tailscale/hskey.txt" = { mode = "0400"; owner = "root"; group = "root"; };
+          "database/password" = { mode = "0400"; owner = "postgres"; group = "postgres"; };
+        };
+      };
+      environment.systemPackages = with pkgs; [ sops age gnupg ];
+      systemd.tmpfiles.rules = [ "d /var/lib/sops-nix 0700 root root" ];
+    };
+  };
 }
