68654e099249603b2670afeb9ad1e6cb56db93ea
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 12:06:02 +1000

    Update remotemouse.nix

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -4,7 +4,6 @@ let
   remotemouse = pkgs.callPackage ../../flake-parts/pkg/remote-mouse.nix { xdotool = pkgs.xdotool; };
 in {
   config = {
-    nixpkgs.overlays = [ (final: prev: { inherit remotemouse; }) ];
     environment.systemPackages = [ remotemouse pkgs.xorg.xhost ];
     nixpkgs.config.allowUnfree = true;
     networking.firewall.allowedTCPPorts = [ 1978 ];
