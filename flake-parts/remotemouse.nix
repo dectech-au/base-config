4f50712619ecede5c78136aacb105f8e0f145b41
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 12:05:38 +1000

    Update remotemouse.nix

--- flake-parts/remotemouse.nix
+++ flake-parts/remotemouse.nix
@@ -1,17 +1,12 @@
-# flake-parts/sops.nix
+# flake-parts/remotemouse.nix
 { inputs, ... }:
 {
   systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
+  
   perSystem = { pkgs, system, ... }:
     let
-      sops = pkgs.sops;
-      age = pkgs.age;
-      gnupg = pkgs.gnupg;
+      remotemouse = pkgs.callPackage ./pkg/remote-mouse.nix { xdotool = pkgs.xdotool; };
     in {
-      packages = {
-        inherit sops age gnupg;
-        default = sops;
-      };
+      packages = { inherit remotemouse; default = remotemouse; };
     };
 }
