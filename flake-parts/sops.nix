c5cf5cce276967e6367721e6471039ca2e28fea6
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 11:50:48 +1000

    Update sops.nix

--- flake-parts/sops.nix
+++ flake-parts/sops.nix
@@ -1,39 +1,12 @@
-# sys-modules/sops.nix
-{ pkgs, sops-nix, ... }:
+# flake-parts/remotemouse.nix
+{ inputs, ... }:
 {
-  config = {
-    nixpkgs.overlays = [ (final: prev: { 
-      inherit (pkgs) sops age gnupg; 
-    }) ];
-    
-    imports = [ sops-nix.nixosModules.sops ];
-    
-    sops = {
-      defaultSopsFile = ../../secrets.yaml;
-      age.keyFile = "/var/lib/sops-nix/key.txt";
-      
-      secrets = {
-        "ssh_host_rsa_key" = {
-          mode = "0400";
-          owner = "root";
-          group = "root";
-        };
-        
-        "tailscale/hskey.txt" = {
-          mode = "0400";
-          owner = "root";
-          group = "root";
-        };
-        
-        "database/password" = {
-          mode = "0400";
-          owner = "postgres";
-          group = "postgres";
-        };
-      };
+  systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  
+  perSystem = { pkgs, system, ... }:
+    let
+      remotemouse = pkgs.callPackage ./pkg/remote-mouse.nix { xdotool = pkgs.xdotool; };
+    in {
+      packages = { inherit remotemouse; default = remotemouse; };
     };
-    
-    environment.systemPackages = with pkgs; [ sops age gnupg ];
-    systemd.tmpfiles.rules = [ "d /var/lib/sops-nix 0700 root root" ];
-  };
 }
