efb7a6b4e5050b4bde52dd8b13a79728f90e9da1
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 11:12:02 +1000

    Update configuration.nix

--- hosts/G531GT-AL017T/configuration.nix
+++ hosts/G531GT-AL017T/configuration.nix
@@ -48,7 +48,6 @@
 		#../../sys-modules/protonmail-bridge.nix
 		#../../sys-modules/protonvpn.nix
 		#../../sys-modules/qt.nix
-		#../../sys-modules/remotemouse.nix
 		../../sys-modules/signal-desktop.nix
 		../../sys-modules/sshfs.nix
 		#../../sys-modules/star-rail-macro.nix
