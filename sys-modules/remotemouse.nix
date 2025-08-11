456172b5564732dea376d1c84f6af08d0484c0a9
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 12:22:00 +1000

    Update remotemouse.nix

--- sys-modules/remotemouse.nix
+++ sys-modules/remotemouse.nix
@@ -1,10 +1,8 @@
 # sys-modules/remotemouse.nix
 { pkgs, ... }:
-let
-  remotemouse = pkgs.callPackage ../../flake-parts/pkg/remote-mouse.nix { xdotool = pkgs.xdotool; };
-in {
+{
   config = {
-    environment.systemPackages = [ remotemouse pkgs.xorg.xhost ];
+    environment.systemPackages = [ pkgs.xorg.xhost ];
     nixpkgs.config.allowUnfree = true;
     networking.firewall.allowedTCPPorts = [ 1978 ];
     networking.firewall.allowedUDPPorts = [ 1978 ];
