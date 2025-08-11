aa59ea6be11c127f3ff45678b8f0b9339e93b076
Declarative_Technologies <declarative-technologies@outlook.com>
2025-08-11 11:49:27 +1000

    Update sops.nix

--- flake-parts/sops.nix
+++ flake-parts/sops.nix
@@ -1,40 +1,12 @@
-# flake-parts/sops.nix
+# flake-parts/remotemouse.nix
 { inputs, ... }:
 {
-  systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-
+  systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  
   perSystem = { pkgs, system, ... }:
     let
-      sops = pkgs.sops;
-      age = pkgs.age;
-      gnupg = pkgs.gnupg;
+      remotemouse = pkgs.callPackage ./pkg/remote-mouse.nix { xdotool = pkgs.xdotool; };
     in {
-      packages = {
-        inherit sops age gnupg;
-        default = sops;
-      };
-      
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          sops
-          age
-          gnupg
-        ];
-        
-        shellHook = ''
-          echo "🔐 Nix-sops development environment loaded"
-          echo "Available tools: sops, age, gpg"
-          echo ""
-          echo "To create a new age key:"
-          echo "  age-keygen -o key.txt"
-          echo ""
-          echo "To encrypt a file:"
-          echo "  sops -e -i secrets.yaml"
-        '';
-      };
+      packages = { inherit remotemouse; default = remotemouse; };
     };
-
-  overlays.default = final: prev: {
-    inherit (inputs.self.packages.${prev.system}) sops age gnupg;
-  };
 }
