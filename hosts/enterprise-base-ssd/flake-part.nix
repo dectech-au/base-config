f933d893ec4ded2783eef01995bbece65b42ae2b
Declarative_Technologies <declarative-technologies@outlook.com>
2025-09-04 13:29:06 +1000

    Update flake-part.nix

--- hosts/enterprise-base-ssd/flake-part.nix
+++ hosts/enterprise-base-ssd/flake-part.nix
@@ -20,7 +20,7 @@
           useUserPackages = true;
           backupFileExtension = "bak";
           users.dectec = import ./home.nix;
-          sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
+          sharedModules = [ inputs.plasma-manager.homeModules.plasma-manager ];
         };
       }
     ];
