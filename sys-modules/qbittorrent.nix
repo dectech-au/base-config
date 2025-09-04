0b1063103a4476ee503dad822d465df030d418ef
Declarative_Technologies <declarative-technologies@outlook.com>
2025-09-04 13:56:18 +1000

    Update qbittorrent.nix

--- sys-modules/qbittorrent.nix
+++ sys-modules/qbittorrent.nix
@@ -1,5 +1,8 @@
 #./sys-modules/qbittorrent.nix
 { config, lib, pkgs, ... }:
 {
-  services.qbittorrent.enable = true;
+  #services.qbittorrent.enable = true;
+  environment.systemPackages = with pkgs; [
+    qbittorrent-enhanced
+  ];
 }
