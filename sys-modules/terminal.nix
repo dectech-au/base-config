f6094937e3f68b22cbaad9318f64c5ad8222e8f4
Declarative_Technologies <declarative-technologies@outlook.com>
2025-10-30 19:38:01 +1100

    Update terminal.nix

--- sys-modules/terminal.nix
+++ sys-modules/terminal.nix
@@ -3,4 +3,14 @@
   environment.systemPackages = with pkgs; [
     yakuake
   ];
+
+  programs = {
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = false;
+        character = { success_symbol = ">"; error_symbol = ">"; };
+      };
+    };
+  };
 }
