bccc2569298792cdd4fa49103e3ae8f9c6370f60
Declarative_Technologies <declarative-technologies@outlook.com>
2025-11-03 11:23:03 +1100

    Update nixvim.nix

--- sys-modules/nixvim.nix
+++ sys-modules/nixvim.nix
@@ -3,44 +3,44 @@
   # No overlays. Keep it simple.
 
   programs.nixvim = {
-    enable = false;
+    enable = true;
 
     # Editor basics
-    #defaultEditor = true;
-    #viAlias = true;
-    #vimAlias = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
 
     # Sensible performance + clipboard for Wayland
-    #performance.byteCompileLua.enable = true;
-    #clipboard.providers.wl-copy.enable = true;
+    performance.byteCompileLua.enable = true;
+    clipboard.providers.wl-copy.enable = true;
 
     # Theme
-    #colorschemes.gruvbox.enable = true;
+    colorschemes.gruvbox.enable = true;
 
     # --- Plugins ---
-    #plugins = {
+    plugins = {
       # Disable LSP entirely to avoid nixvim's atopile option default hitting a missing pkgs.atopile.
       # Re-enable later when your pin includes it.
-      #lsp.enable = false;
+      lsp.enable = false;
 
       # Useful, low-risk UI/UX plugins
-      #treesitter = {
-      #  enable = true;
-      #  settings.indent.enable = true;
-      #};
+      treesitter = {
+        enable = true;
+        settings.indent.enable = true;
+      };
 
-      #notify.enable = true;
-      #lualine.enable = true;
-      #telescope.enable = true;
-      #web-devicons.enable = true;
-      #bufferline.enable = true;
-      #gitsigns.enable = true;
-      #comment.enable = true;
-      #nvim-autopairs.enable = true;
-      #indent-blankline.enable = true;
+      notify.enable = true;
+      lualine.enable = true;
+      telescope.enable = true;
+      web-devicons.enable = true;
+      bufferline.enable = true;
+      gitsigns.enable = true;
+      comment.enable = true;
+      nvim-autopairs.enable = true;
+      indent-blankline.enable = true;
 
       # Git tooling inside Neovim
-      #fugitive.enable = true;
+      fugitive.enable = true;
 
       # If you later re-enable LSP, this is how to keep dockerls off without overlays:
       # lsp = {
@@ -56,30 +56,30 @@
       #     # pylsp.enable = true;
       #   };
       # };
-    #};
+    };
 
     # --- Core options ---
-    #opts = {
-    #  number = true;
-    #  undofile = true;
+    opts = {
+      number = true;
+      undofile = true;
 
-      #shiftwidth = 2;
-      #tabstop = 2;
-      #softtabstop = 2;
-      #expandtab = true;
-      #smartindent = true;
-      #autoindent = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      softtabstop = 2;
+      expandtab = true;
+      smartindent = true;
+      autoindent = true;
 
-      #clipboard = "unnamedplus";
-      #ignorecase = true;
-      #smartcase = true;
-      #signcolumn = "yes:1";
-      #termguicolors = true;
-      #scrolloff = 5;
-      #splitright = true;
-      #splitbelow = true;
-      #guicursor = "n-v-c:blinkon0";
-    #};
+      clipboard = "unnamedplus";
+      ignorecase = true;
+      smartcase = true;
+      signcolumn = "yes:1";
+      termguicolors = true;
+      scrolloff = 5;
+      splitright = true;
+      splitbelow = true;
+      guicursor = "n-v-c:blinkon0";
+    };
 
     # Example: later, when LSP is back, you can add keymaps or extra Lua here.
     # extraConfigLua = ''
