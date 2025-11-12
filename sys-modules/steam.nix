d789f8584bcdb68415b05acd3c965fd85ee84c0d
Declarative_Technologies <declarative-technologies@outlook.com>
2025-11-13 08:59:05 +1100

    Update steam.nix

--- sys-modules/steam.nix
+++ sys-modules/steam.nix
@@ -1,7 +1,12 @@
 #~/.dotfiles/modules/steam.nix
 { config, lib, pkgs, ... }:
 {
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   systemd.user.services.steam-autostart = {
     enable = true;
@@ -13,6 +18,36 @@
     };
   };
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      mesa.drivers
+      mesa.vulkan-drivers
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      mesa.drivers
+      mesa.vulkan-drivers
+    ];
+  };
+
+  hardware.nvidia = {
+    open = false;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    prime = {
+      offload.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+  };
+
+  environment.sessionVariables = {
+    STEAM_RUNTIME = "1";
+  };
+
   environment.systemPackages = with pkgs; [
     protonup-ng
     protonup-qt
@@ -20,3 +55,11 @@
     steamtinkerlaunch
   ];
 }
+
+
+    # Prevent Steam from mangling runtime
+    STEAM_RUNTIME = "0";
+    # Prefer system Vulkan drivers
+    VK_ICD_FILENAMES = "${pkgs.mesa.drivers}/share/vulkan/icd.d/nvidia_icd.json";
+    # For Steam controller driver support
+    SDL_GAMECONTROLLERCONFIG = "/etc/SDLGameControllerDB.txt";
