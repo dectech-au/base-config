b0132187908392785578a3ea4b6b11471454827b
Declarative_Technologies <declarative-technologies@outlook.com>
2025-11-13 08:47:54 +1100

    Update steam.nix

--- sys-modules/steam.nix
+++ sys-modules/steam.nix
@@ -3,6 +3,16 @@
 {
   programs.steam.enable = true;
 
+  systemd.user.services.steam-autostart = {
+    enable = true;
+    description = "Auto-start Steam on login";
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.steam}/bin/steam";
+      Restart = "on-failure";
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     protonup-ng
     protonup-qt
