3cb72d39f0d2461f4b3675a6d460ad5257ff4170
Declarative_Technologies <declarative-technologies@outlook.com>
2025-11-20 07:19:27 +0000

    Create trezor.nix

---
+++ sys-modules/trezor.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+  trezor-suite
+  trezorctl
+  ];
+
+  services.trezord.enable = true;
+  services.udev.packages = with pkgs; [ trezor-udev-rules ];
+}
