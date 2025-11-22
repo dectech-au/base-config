68150720caf79846001686e1b95a25191774d4c5
Declarative_Technologies <declarative-technologies@outlook.com>
2025-11-22 19:14:47 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "DecTec default flake";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     
     flake-parts.url = "github:hercules-ci/flake-parts";
     #flake-parts.inputs.nixpkgs.follows = "nixpkgs";
