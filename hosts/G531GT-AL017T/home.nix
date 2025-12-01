51540b50e58e554282d4c2a2a75cd8f5f8dec527
Declarative_Technologies <declarative-technologies@outlook.com>
2025-12-02 10:14:53 +1100

    Update home.nix

--- hosts/G531GT-AL017T/home.nix
+++ hosts/G531GT-AL017T/home.nix
@@ -36,13 +36,6 @@
   home.homeDirectory = "/home/dectec";
   home.stateVersion = "25.05"; # Do not change this after initial setup
 
-
-  programs.git = {
-    enable = true;
-    userName = "DecTec";
-    userEmail = "zozano@protonmail.com";
-  };
-
   # home.file.".local/share/applications/teams.desktop".text = ''
   #   test
   # '';
