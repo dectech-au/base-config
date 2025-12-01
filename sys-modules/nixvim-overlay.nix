4670cc7a4fb13cf8c97a4cff1a8b60cdcda28c9a
Declarative_Technologies <declarative-technologies@outlook.com>
2025-12-02 10:00:32 +1100

    Update nixvim-overlay.nix

--- sys-modules/nixvim-overlay.nix
+++ sys-modules/nixvim-overlay.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   nixpkgs.overlays = [
     (final: prev: {
-      dockerfile-language-server = prev.nodePackages.dockerfile-language-server-nodejs;
+      dockerfile-language-server = prev.nodePackages.dockerfile-language-server;
     })
   ];
 }
