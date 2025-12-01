18462d58675c15d1187b576091fd509eb30f5458
Declarative_Technologies <declarative-technologies@outlook.com>
2025-12-02 10:02:50 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "DecTec default flake";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     
     flake-parts.url = "github:hercules-ci/flake-parts";
     #flake-parts.inputs.nixpkgs.follows = "nixpkgs";
@@ -22,10 +22,10 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
-    nixvim.url = "github:nix-community/nixvim/nixos-25.05";
+    nixvim.url = "github:nix-community/nixvim/nixos-25.11";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
