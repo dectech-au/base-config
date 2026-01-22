7cb6674adb1040dee394f70fd7b88b4e234827f6
Declarative_Technologies <declarative-technologies@outlook.com>
2026-01-22 23:10:06 +0000

    Update server-mount.nix

--- sys-modules/server-mount.nix
+++ sys-modules/server-mount.nix
@@ -2,13 +2,13 @@
 {
   fileSystems = {
     "/server" = {
-      device = "z-home@192.168.1.157:/";
+      device = "z-home@z-home-mac:/";
       fsType = "sshfs";
       options = [
         "nodev"
         "nofail"
         "allow_other"
-        "IdentityFile=/root/.ssh/id_ed25519_nixos"
+        "IdentityFile=/root/.ssh/home-server_z-nix"
         "x-systemd.automount"
         "x-systemd.requires=network-online.target"
       ];
