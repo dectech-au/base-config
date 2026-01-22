3796a1ca63e29141990ac8c021cda3a032976730
Declarative_Technologies <declarative-technologies@outlook.com>
2026-01-22 22:27:41 +0000

    Update sshfs.nix

--- sys-modules/sshfs.nix
+++ sys-modules/sshfs.nix
@@ -8,3 +8,17 @@
 
 # how to use:
 # sshfs remote_user@machine:/remote/directory ~/local/directory
+#
+# or declaratively:
+#     "/server" = {
+#      device = "z-home@192.168.1.157:/";
+#      fsType = "sshfs";
+#      options = [
+#        "nodev"
+#        "nofail"
+#        "allow_other"
+#        "IdentityFile=/root/.ssh/id_ed25519_nixos"
+#        "x-systemd.automount"
+#        "x-systemd.requires=network-online.target"
+#      ];
+#    };
