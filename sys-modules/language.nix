3e14e9c9c37d37020dbdaee62ba6fbf4e4c1b4a3
Declarative_Technologies <declarative-technologies@outlook.com>
2026-03-04 23:30:45 +0000

    Update language.nix

--- sys-modules/language.nix
+++ sys-modules/language.nix
@@ -5,7 +5,7 @@
     packages = with pkgs; [
       noto-fonts-cjk-sans  # covers Hangul well
       nanum                # common Korean font family (extra coverage)
-      noto-fonts-emoji     # optional, helps with emoji in chats
+      noto-fonts-color-emoji     # optional, helps with emoji in chats
     ];
   };
 }
