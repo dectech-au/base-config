9dfe8b6741cb742fb38eea14878771840091f6a2
Declarative_Technologies <declarative-technologies@outlook.com>
2026-05-17 00:14:27 +0000

    Update steam.nix

--- sys-modules/steam.nix
+++ sys-modules/steam.nix
@@ -1,24 +1,6 @@
 #~/.dotfiles/modules/steam.nix
 { config, lib, pkgs, ... }:
 {
-  services.sunshine = {
-    enable = true;
-    openFirewall = true;
-    #autoStart = true; 
-    #capSysAdmin = true;
-    settings.sunshine_name = "nixos-laptop";
-    #package = pkgs.sunshine.override {
-    #  cudaSupport = true;
-    #  cudaPackages = pkgs.cudaPackages;
-    #};
-  };
-
-#services.avahi.publish.enable = true;
-#services.avahi.publish.userServices = true;
-#hardware.uinput.enable = true;
-#users.users.leo.extraGroups = [ "uinput" ];
-
-
   programs.steam = {
     enable = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
