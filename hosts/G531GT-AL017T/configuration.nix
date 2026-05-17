b9e07c725f7b3e76c7b42ea128f340c8b3a78175
Declarative_Technologies <declarative-technologies@outlook.com>
2026-05-17 00:32:55 +0000

    Update configuration.nix

--- hosts/G531GT-AL017T/configuration.nix
+++ hosts/G531GT-AL017T/configuration.nix
@@ -66,6 +66,7 @@
 		../../sys-modules/sshfs.nix
 		#../../sys-modules/star-rail-macro.nix
 		../../sys-modules/steam.nix
+		../../sys-modules/sunshine.nix
 		../../sys-modules/tailscale.nix
 		#../../sys-modules/teams.nix
 		../../sys-modules/teamviewer.nix
