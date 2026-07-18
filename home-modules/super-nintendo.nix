dce3f5372b236912bb4366e9d6867da971d09a0e
Declarative_Technologies <declarative-technologies@outlook.com>
2026-07-19 09:50:38 +1000

    Update and rename super-nintendo.nix to super-nintendo.nix

--- home-modules/super-nintendo.nix
+++ home-modules/super-nintendo.nix
@@ -1,7 +1,8 @@
-home-manager.users.zozano = {
+{ config, lib, pkgs, ... }:
+{
   home.packages = [
     (pkgs.retroarch.override {
       cores = with pkgs.libretro; [ bsnes snes9x ];
     })
   ];
-};
+}
