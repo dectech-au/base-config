96db1d40b1e00b828e22d130202578bed8c6c818
Declarative_Technologies <declarative-technologies@outlook.com>
2026-08-23 11:01:43 +1000

    Update gnome.nix

--- sys-modules/gnome.nix
+++ sys-modules/gnome.nix
@@ -4,11 +4,11 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "leo";
-  };
+  #services.displayManager.autoLogin = {
+  #  enable = true;
+  #  user = "leo";
+  #};
 
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  #systemd.services."getty@tty1".enable = false;
+  #systemd.services."autovt@tty1".enable = false;
 }
