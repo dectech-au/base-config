3463185c24f5c391328965dd26614c5d7e811dd2
Declarative_Technologies <declarative-technologies@outlook.com>
2026-08-29 13:45:39 +1000

    Update gnome.nix

--- sys-modules/gnome.nix
+++ sys-modules/gnome.nix
@@ -10,24 +10,12 @@
 
   services.desktopManager.gnome.enable = true;
 
-  environment.systemPackages = with pkgs; [ 
-    adwaita-icon-theme
-    vanilla-dmz
-  ];
-
-  home-manager.users.dectec.home.pointerCursor = {
-    enable = true;
-    name = "Vanilla-DMZ";
-    package = [ pkgs.vanilla-dmz ];
-    size = 32;
-  };
-
   services.displayManager.autoLogin = {
     enable = true;
     user = "leo";
   };
 
-  services.udev.extraRules = ''
-    SUBSYSTEM=="drm", KERNEL=="card*", DRIVERS=="i915", TAG+="mutter-device-preferred-primary"
-  '';
+  #services.udev.extraRules = ''
+  #  SUBSYSTEM=="drm", KERNEL=="card*", DRIVERS=="i915", TAG+="mutter-device-preferred-primary"
+  #'';
 }
