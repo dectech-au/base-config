6113355b84882deb00f27985a828b8e74f787b9e
Declarative_Technologies <declarative-technologies@outlook.com>
2026-08-29 11:29:13 +1000

    Update gnome.nix

--- sys-modules/gnome.nix
+++ sys-modules/gnome.nix
@@ -2,10 +2,15 @@
 { ... }:
 
 {
-  services.displayManager.gdm.enable = true;
+  services.displayManager = {
+    gdm.enable = true;
+    after = [ "systemd-udev-settle.service" ];
+    wants = [ "systemd-udev-settle.service" ];
+  };
+
   services.desktopManager.gnome.enable = true;
 
-  services.udev.extraRules = ''
-    SUBSYSTEM=="drm", KERNEL=="card*", DRIVERS=="i915", TAG+="mutter-device-preferred-primary"
-  '';
+  #services.udev.extraRules = ''
+  #  SUBSYSTEM=="drm", KERNEL=="card*", DRIVERS=="i915", TAG+="mutter-device-preferred-primary"
+  #'';
 }
