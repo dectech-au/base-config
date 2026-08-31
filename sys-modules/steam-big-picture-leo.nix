3035e36c133a5480c410bfaba2ff28430e456d31
Declarative_Technologies <declarative-technologies@outlook.com>
2026-08-31 14:00:04 +1000

    Update steam-big-picture-leo.nix

--- sys-modules/steam-big-picture-leo.nix
+++ sys-modules/steam-big-picture-leo.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  home-manager.users.leo.home.stateVersion = "25.05";
-
   home-manager.users.leo.systemd.user.services.steam-bigpicture = {
     Unit = {
       Description = "Autostart Steam in Big Picture mode once XWayland is ready";
